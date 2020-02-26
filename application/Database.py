e7f1e489073a5b8450dd3d58717d826dc1ebe5aa
ThaRising <kochbe.ber@gmail.com>
2020-02-26 18:35:53 +0100

    Expanded database interface

--- application/Database.py
+++ application/Database.py
@@ -1,6 +1,22 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError
+from typing import Optional
+from .models import Base
 
 
 class Database:
     def __init__(self, uri: str = "sqlite:///:memory:"):
         self.engine = create_engine(uri)
+        Base.metadata.create_all(self.engine)
+        self.Session = sessionmaker(bind=self.engine)
+
+    def add(self, object_: any) -> Optional[str]:
+        session = self.Session()
+        try:
+            session.add(object_)
+            session.commit()
+            return str(object_)
+        except IntegrityError:
+            session.rollback()
+            return IntegrityError
